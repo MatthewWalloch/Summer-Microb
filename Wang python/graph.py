394ee63158e026bc5deeb9d9172cb6bf825bc2a8
MatthewWalloch <105795240+MatthewWalloch@users.noreply.github.com>
2024-06-13 14:43:08 -0400

    making graphs work (need labels and last 50 gen

--- Wang python/graph.py
+++ Wang python/graph.py
@@ -1,33 +1,82 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
 import json
+import os
 
-filename = "Wang python\json\Thu Jun 13 13-08-41 2024 0.5 500000000.0 0 0.0001 False 5000.json"
-with open(filename, "r") as f:
-    data = json.load(f)
+
+# 
+
+# filename = "Wang python\json\Thu Jun 13 13-49-13 2024 0.5 6000000000 0 0.0001 False 5000.json"
+# with open(filename, "r") as f:
+#     data = json.load(f)
+# fig, ax = plt.subplots(4,2)
+# max_G= len(data["fit_Evo"])
+# ax[0,0].plot(range(max_G), data["fit_Evo"])
+# ax[0,0].set_title("fit_Evo")
+
+# ax[1,0].plot(range(max_G), data["pro_Rate_Evo"])
+# ax[1,0].set_title("pro_Rate_Evo")
+
+# ax[2,0].plot(range(max_G), data["sig_Th_Evo"])
+# ax[2,0].set_title("sig_Th_Evo")
+
+# ax[0,1].plot(range(max_G), data["coopPayoff_Evo"])
+# ax[0,1].set_title("coopPayoff_Evo")
+
+# ax[1,1].plot(range(max_G), data["sigCost_Evo"])
+# ax[1,1].set_title("sigCost_Evo")
+
+# ax[2,1].plot(range(max_G), data["coopCost_Evo"])
+# ax[2,1].set_title("coopCost_Evo")
+
+# ax[3,1].plot(range(max_G), data["auto_pro_Rate_Evo"])
+# ax[3,1].set_title("auto_pro_Rate_Evo")
+# plt.tight_layout()
+# plt.show()
+
+
+cm = plt.get_cmap("plasma")
+cNorm = colors.Normalize(vmin=5, vmax=110)
+scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
 fig, ax = plt.subplots(4,2)
-max_G= len(data["fit_Evo"])
-ax[0,0].plot(range(max_G), data["fit_Evo"])
-ax[0,0].set_title("fit_Evo")
+max_G= 5000
 
-ax[1,0].plot(range(max_G), data["pro_Rate_Evo"])
-ax[1,0].set_title("pro_Rate_Evo")
 
-ax[2,0].plot(range(max_G), data["sig_Th_Evo"])
-ax[2,0].set_title("sig_Th_Evo")
 
-ax[3,0].plot(range(max_G), data["auto_R_Evo"])
-ax[3,0].set_title("auto_R_Evo")
+for path, directories, files in os.walk("Wang python\json\Fig1"):
+    for file in files:
+        cost = int(file.split(" ")[6]) // 10** 8
+        filename = path+"\\"+file
+        with open(filename, "r") as f:
+            data = json.load(f)
+        ax[3,1].plot(range(max_G), data["fit_Evo"], color=scalarMap.to_rgba(cost))
+        ax[3,1].set_title("fit_Evo")
 
-ax[0,1].plot(range(max_G), data["coopPayoff_Evo"])
-ax[0,1].set_title("coopPayoff_Evo")
+        ax[2,0].plot(range(max_G), data["pro_Rate_Evo"], color=scalarMap.to_rgba(cost))
+        ax[2,0].set_title("pro_Rate_Evo")
 
-ax[1,1].plot(range(max_G), data["sigCost_Evo"])
-ax[1,1].set_title("sigCost_Evo")
+        ax[3,0].plot(range(max_G), data["sig_Th_Evo"], color=scalarMap.to_rgba(cost))
+        ax[3,0].set_title("sig_Th_Evo")
 
-ax[2,1].plot(range(max_G), data["coopCost_Evo"])
-ax[2,1].set_title("coopCost_Evo")
+        ax[0,1].plot(range(max_G), data["coopPayoff_Evo"], color=scalarMap.to_rgba(cost))
+        ax[0,1].set_title("coopPayoff_Evo")
 
-ax[3,1].plot(range(max_G), data["auto_pro_Rate_Evo"])
-ax[3,1].set_title("auto_pro_Rate_Evo")
+        ax[1,1].plot(range(max_G), data["sigCost_Evo"], color=scalarMap.to_rgba(cost))
+        ax[1,1].set_title("sigCost_Evo")
+
+        ax[2,1].plot(range(max_G), data["coopCost_Evo"], color=scalarMap.to_rgba(cost))
+        ax[2,1].set_title("coopCost_Evo")
+
+    
+
+    print(path)
+
+ax[0,0].bar([i for i in range(5,105, 5)], 
+        [1 for i in range(5,105,5)], 
+        width=5,
+        tick_label=[i for i in range(5,105, 5)],
+        color=[scalarMap.to_rgba(i) for i in range(5,105, 5)])
+ax[0,0].set_xlabel("Cost of signaling (10^8 FU)")
 plt.tight_layout()
 plt.show()
