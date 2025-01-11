cdae2807974c7e63b9ce0179a2f6e5aa44ce1c94
MatthewWalloch <105795240+MatthewWalloch@users.noreply.github.com>
2025-01-11 12:16:41 -0500

    new

---
+++ Spring 25 Exloration/helpers.py
@@ -0,0 +1,102 @@
+import numpy as np
+import time 
+import joblib
+import random
+
+
+
+def mut_parameter(mut_vector, mut_P, mut_SD, mut_Min, mut_Max, index_Cheats,size_Pop):
+    # mutation operation for evolving traits number chosen based on poission on mut_P and mutated 
+    # based on truncated normal based on mut_SD, mut_Min, mut_Max with mean of unmutated value
+    # mut_vector, index_Cheats is list of (size_Pop,) 
+    # mut_P, mut_SD, mut_Min, mut_Max are Floats
+    # size_Pop is Int
+    # returns new mut_vector of size (size_Pop,)
+    num_Mut = np.random.poisson(mut_P*size_Pop)
+    if num_Mut !=0:
+        index_Mut = np.random.choice(size_Pop, size=num_Mut, replace=False)
+        index_Diff = [i for i in index_Mut if index_Cheats[i] != 1]
+        for i in index_Diff:
+            mutation =  np.random.normal(mut_vector[i], mut_SD)
+            if mutation < mut_Min:
+                mutation = mut_Min
+            if mutation > mut_Max:
+                mutation = mut_Max
+            mut_vector[i] = mutation
+    return mut_vector
+
+
+def sample_ztp(lam):
+    # zero-truncated Poisson distribution based on lambda, returns int
+    u = np.random.uniform(np.exp(-lam), 1)
+    t = -np.log(u)
+    return 1 + np.random.poisson(lam - t)
+
+
+def eval_genotype_No_Auto_Clonal(fit_Pop,coopPayoff_Pop,coopCost_Pop,sigCost_Pop,
+    pro_Rate,sensitivity,gp):
+
+    contribute = pro_Rate / decay_Rate 
+    contribute_Matrix = np.full((grid_Size,size_Pop), contribute).transpose()
+    den_Matrix = np.full((grid_Size, grid_Size), env_CellDen)
+
+    H_C_g_j = sensitivity * (env_CellDen * contribute_Matrix).transpose() 
+    H_C_g_j = H_C_g_j.transpose()
+    cost_sum =  H_C_g_j.dot(np.ones(grid_Size)) * coop_Cost
+    H_B_g_j = (H_C_g_j * env_CellDen) > np.full((size_Pop,grid_Size), median_CellDen)
+    benifit_sum = H_B_g_j.dot(np.ones(grid_Size)) * coop_Benefit
+    signal_cost = pro_Rate * sig_Cost
+    fitness = np.full((size_Pop,), baseline) + benifit_sum - cost_sum - signal_cost
+
+    return benifit_sum, cost_sum, signal_cost, fitness
+
+def eval_genotype_No_Auto(fit_Pop,coopPayoff_Pop,coopCost_Pop,sigCost_Pop,
+    pro_Rate,sensitivity,baseline,coop_Benefit,coop_Cost,sig_Cost,size_Pop,lam,env_CellDen,
+    grid_Size,base_Volume,decay_Rate,median_CellDen):
+    rng = np.random.default_rng()    
+        
+    # conbined_rates = np.array(joblib.Parallel(n_jobs= 4)(joblib.delayed(random_parallel)( i, rng, lam, size_Pop, pro_Rate, sensitivity, env_CellDen, grid_Size, median_CellDen, decay_Rate) for i in range(size_Pop)))
+
+    benifit_sum = np.zeros(size_Pop)
+    cost_sum = np.zeros(size_Pop)
+    signal_cost = np.zeros(size_Pop)
+    for i in range(size_Pop):
+        mix_Num = sample_ztp(lam)
+        indexes = np.array(np.append([i], rng.integers(0, high=size_Pop, size=(mix_Num-1,))))
+        # bellow is faster but "less random"
+        # indexes = np.array(np.append([i], random.choices(all_index, k=mix_Num-1)), dtype=int)
+
+        conbined_rates = np.average(pro_Rate[indexes]) / decay_Rate
+        den_Matrix = np.full((mix_Num, grid_Size), env_CellDen).transpose()
+
+        contribute_matrix = conbined_rates * den_Matrix
+        H_C_g_j = (sensitivity[indexes] * contribute_matrix)
+        cost_sum[i] = H_C_g_j.transpose().dot(np.ones(grid_Size))[0] * coop_Cost
+        H_B_g_j = (env_CellDen * H_C_g_j.transpose()) > np.full((mix_Num,grid_Size), median_CellDen)
+        # H_B_g_j = (env_CellDen * H_C_g_j.transpose() - 10**1.5) / (10**5-10**1.5)
+        benifit_sum[i] = np.sum((H_B_g_j.transpose().dot(np.ones(mix_Num)) / mix_Num)) * coop_Benefit
+        signal_cost[i] = pro_Rate[i] * sig_Cost
+    # signal_cost = pro_Rate * sig_Cost
+    fitness = np.full((size_Pop,), baseline) + benifit_sum - cost_sum - signal_cost
+
+
+
+
+# testing code:
+if __name__ == "__main__":
+    rng = np.random.default_rng()
+    print(rng.integers(0, high=10))
+    print(time.strftime("%d-%m %H-%M-%S"))
+    # test = np.array([1,2,3])
+    # test2 = np.array([4,5,6,7])
+    # print(test / [1,2,3])
+    # thresholds = np.array([[9,8],[10,0],[11,0]])
+    # matrix = np.full((4,3), test).transpose()
+    # # print(matrix)
+    # matrix2 = np.full((4,4), test2)
+    # product = matrix.dot(matrix2) / 4
+    # print(product)
+    # threshold_matrix = np.full((4,3), thresholds).transpose()
+    # print(threshold_matrix)
+    # print(product > threshold_matrix)
+    # print(((product > threshold_matrix) * test2)> np.full((3,4), 5.5))
---
+++ Spring 25 Exloration/main.py
@@ -0,0 +1,260 @@
+import numpy as np
+import time
+from helpers import *
+import matplotlib.pyplot as plt
+import json
+import joblib
+
+
+
+def main_QS(init_SN, sig_Cost ,lam , K, Auto=False, max_G=500, clonal=True):
+    ################################################################################
+    # set parameters
+    ################################################################################
+    # population size
+    # baseline,coop_Benefit,coop_Cost,sig_Cost,size_Pop,lam,env_CellDen,grid_Size,base_Volume,decay_Rate,median_CellDen
+    # maximum cellular density (cells per microliter)
+    max_CellDen = 10.0 ** 5
+    # minimum cellular density
+    min_CellDen = 10.0 ** 1.5
+    size_Pop = 5000
+    grid_Size= 100
+    gp = {
+        "baseline": 100,
+        "coop_Benefit": 1.5,
+        "coop_Cost": .5,
+        "sig_Cost": sig_Cost,
+        "size_Pop": size_Pop,
+        "lam": lam,
+        "env_CellDen": np.array(list(np.linspace(min_CellDen, max_CellDen,num=grid_Size))),
+        "grid_Size": 100,
+        "decay_Rate": 10.0 ** -4.0,
+        "median_CellDen": (max_CellDen + min_CellDen) * .5
+    }
+    # mutation rate
+    mu_Production = 0.01
+    mu_sensitiity = 0.01
+    mu_R = 0.01
+    # print("mutation rates increased for testing")
+    # mu_Production = 0.1
+    # mu_sensitiity = 0.1
+    # mu_R = 0.1
+
+    
+ 
+   
+    # maximum cellular production rate
+    max_ProRate = 2e-08
+    # minimum cellular production rate
+    min_ProRate = 0
+    # initial cellular production rate
+    init_pro_Rate = 0.5e-08 
+    # SD for mutation of cellular production rate
+    mu_SD_ProRate = 0.01e-08
+
+    # maximum signal concentration
+    max_sensitivity = 20
+    # minimum signal concentration
+    min_sensitivity = 0.00000001
+    # initial signal threshold
+    # init_SN = .01
+    # SD for mutation of signal concentration
+    mu_SD_sensitivity = .01
+
+    # maximum ratio of autoinduction production
+    max_R = 20.
+    # minimum ratio of autoinduction production
+    min_R = 0.01
+    # initial ratio of autoinduction production
+    init_R = 5.
+    # SD for mutation of ratio of autoinduction production
+    mu_SD_R = 1.0
+
+
+    ################################################################################
+    # initialization
+    ################################################################################
+    # initialize production rate
+    pro_Rate = np.full((size_Pop,), init_pro_Rate)
+    # initialize signal threshold
+    sensitivity = np.full((size_Pop,), init_SN)
+    # initialize ratio of autoinduction production
+    auto_R = np.full((size_Pop,), init_R)
+    # initialize genotype fitness
+    fit_Pop = np.zeros((size_Pop,))
+    # initialize cooperation payoff
+    coopPayoff_Pop = np.zeros(size_Pop)
+    # initialize cost for signaling
+    sigCost_Pop = np.zeros(size_Pop)
+    # initialize cost for cooperation
+    coopCost_Pop = np.zeros(size_Pop)
+    # autoinduction production rate
+    auto_pro_Rate = np.zeros(size_Pop)
+    # efficiency for cooperation
+
+
+    # initialize result saving space
+    # all lineages
+    fit_Evo =  np.zeros(max_G)
+    pro_Rate_Evo =  np.zeros(max_G)
+    sensitivity_Evo =  np.zeros(max_G)
+    auto_R_Evo =  np.zeros(max_G)
+    coopPayoff_Evo =  np.zeros(max_G)
+    sigCost_Evo =  np.zeros(max_G)
+    coopCost_Evo =  np.zeros(max_G)
+    coopEff_Evo =  np.zeros(max_G)
+    auto_pro_Rate_Evo =  np.zeros(max_G)
+
+    # record cheats
+    numCheats_Evo =  np.zeros(max_G)
+    ################################################################################
+    # Evolution
+    ################################################################################
+    # initial evaluation
+
+    # eval_genotype
+    eval_genotype_No_Auto_Clonal(fit_Pop,coopPayoff_Pop,coopCost_Pop,sigCost_Pop,
+    pro_Rate,sensitivity,gp)
+
+    g = 0
+    fit_Evo[g] = np.mean(fit_Pop)
+    pro_Rate_Evo[g] = np.mean(pro_Rate)
+    sensitivity_Evo[g] = np.mean(sensitivity)
+    auto_R_Evo[g] = np.mean(auto_R)
+    coopPayoff_Evo[g] = np.mean(coopPayoff_Pop)
+    sigCost_Evo[g] = np.mean(sigCost_Pop)
+    coopCost_Evo[g] = np.mean(coopCost_Pop)
+    auto_pro_Rate_Evo[g] = np.mean(auto_pro_Rate)
+
+    t= time.time_ns()
+    for g in range(1,max_G):
+
+        temp_pro_Rate = np.zeros(size_Pop)
+        temp_sensitivity = np.zeros(size_Pop)
+        temp_fit_Pop = np.zeros(size_Pop)
+        temp_coopPayoff_Pop = np.zeros(size_Pop)
+        temp_coopCost_Pop = np.zeros(size_Pop)
+        temp_sigCost_Pop = np.zeros(size_Pop)
+        temp_auto_R = np.zeros(size_Pop)
+        temp_auto_pro_Rate = np.zeros(size_Pop)
+        # print(fit_Evo)
+        for i in range(len(fit_Pop)):
+            if fit_Pop[i] < 0:
+                fit_Pop[i] = 0
+        fit_pop_probability = fit_Pop
+        fit_pop_probability = fit_pop_probability / sum(fit_pop_probability)
+        index_Select = np.random.choice(size_Pop, size=size_Pop, p=fit_pop_probability)
+
+        for n in range(len(index_Select)):
+            temp_pro_Rate[n] = pro_Rate[int(index_Select[n])]
+            temp_sensitivity[n] = sensitivity[int(index_Select[n])]
+            temp_fit_Pop[n] = fit_Pop[int(index_Select[n])]
+            temp_coopPayoff_Pop[n] = coopPayoff_Pop[int(index_Select[n])]
+            temp_coopCost_Pop[n] = coopCost_Pop[int(index_Select[n])]
+
+            temp_sigCost_Pop[n] = sigCost_Pop[int(index_Select[n])]
+            temp_auto_R[n] = auto_R[int(index_Select[n])]
+            temp_auto_pro_Rate[n] = auto_pro_Rate[int(index_Select[n])]
+        
+        pro_Rate = temp_pro_Rate
+        sensitivity = temp_sensitivity
+        fit_Pop = temp_fit_Pop
+        coopPayoff_Pop = temp_coopPayoff_Pop 
+        coopCost_Pop = temp_coopCost_Pop
+        igCost_Pop = temp_sigCost_Pop
+        auto_pro_Rate = temp_auto_pro_Rate
+        auto_R = temp_auto_R
+        
+        # mutate production rate
+        pro_Rate = mut_parameter(pro_Rate,mu_Production,mu_SD_ProRate,min_ProRate,max_ProRate,size_Pop)
+        # mutate signal threshold
+        sensitivity = mut_parameter(sensitivity,mu_sensitiity,mu_SD_sensitivity,min_sensitivity,max_sensitivity,size_Pop)
+        # mutate ratio of autoinduction production
+        auto_R = mut_parameter(auto_R,mu_R,mu_SD_R,min_R,max_R,size_Pop)
+        # save results  0 sec ish
+        fit_Evo[g] = np.mean(fit_Pop)
+        pro_Rate_Evo[g] = np.mean(pro_Rate)
+        sensitivity_Evo[g] = np.mean(sensitivity)
+        auto_R_Evo[g] = np.mean(auto_R)
+        coopPayoff_Evo[g] = np.mean(coopPayoff_Pop)
+        sigCost_Evo[g] = np.mean(sigCost_Pop)
+        coopCost_Evo[g] = np.mean(coopCost_Pop)
+        auto_pro_Rate_Evo[g] = np.mean(auto_pro_Rate)
+
+        
+        
+        # genotype evaluation
+        
+        if g % 500 == 499:
+            print(f"{lam}: {g+1}    {(time.time_ns()-t)* 10 **-9}")
+            # t = time.time_ns()
+            # data = {"fit_Pop": fit_Pop.tolist(),
+            #         "pro_Rate": pro_Rate.tolist(),
+            #         "sensitivity": sensitivity.tolist(),
+            #         "auto_R": auto_R.tolist(),
+            #         "coopPayoff_Pop": coopPayoff_Pop.tolist(),
+            #         "sigCost_Pop": sigCost_Pop.tolist(),
+            #         "coopCost_Pop": coopCost_Pop.tolist(),
+            #         "auto_pro_Rate": auto_pro_Rate.tolist()}
+            # file = f"New python\json\log mult sense 10k\gen {g+1}.json"
+            # with open(file, "w") as f:
+            #     json.dump(data, f,  ensure_ascii=False, indent=4)
+    timestr = time.strftime("%d-%m %H-%M-%S")
+    # if Auto:
+    #     file = f"New python\\auto json\\{timestr} {lam} {init_SN} {sig_Cost} {max_G} {clonal}.json"
+    # else:
+    #     file = f"New python\json\log many geno\\{timestr} {lam} {init_SN} {sig_Cost} {max_G} {clonal}.json"
+    
+    data = {"fit_Evo": fit_Evo.tolist(),
+            "pro_Rate_Evo": pro_Rate_Evo.tolist(),
+            "sensitivity_Evo": sensitivity_Evo.tolist(),
+            "auto_R_Evo": auto_R_Evo.tolist(),
+            "coopPayoff_Evo": coopPayoff_Evo.tolist(),
+            "sigCost_Evo": sigCost_Evo.tolist(),
+            "coopCost_Evo": coopCost_Evo.tolist(),
+            "auto_pro_Rate_Evo": auto_pro_Rate_Evo.tolist()}
+    # with open(file, "w") as f:
+    #     json.dump(data, f,  ensure_ascii=False, indent=4)
+    return file
+
+
+def vary_signal(sig_Cost, auto, clonal):
+
+    # t = time.time_ns()
+    coop_Cost ,lam , K, mu_Cheats, max_G = 0.5, 0, 50.0, 10.0 ** -4, 5000
+    returnValues = main_QS(coop_Cost, sig_Cost ,lam , K, mu_Cheats, max_G=max_G, Auto=auto, clonal=clonal)
+    fit_Evo, pro_Rate_Evo, sensitivity_Evo, auto_R_Evo, coopPayoff_Evo, sigCost_Evo, coopCost_Evo, auto_pro_Rate_Evo = returnValues
+    # print((time.time_ns()-t)* 10 **-9)
+    print(f'{sig_Cost // 10 ** 8} done') 
+    data = {"fit_Evo": fit_Evo.tolist(),
+            "pro_Rate_Evo": pro_Rate_Evo.tolist(),
+            "sensitivity_Evo": sensitivity_Evo.tolist(),
+            "auto_R_Evo": auto_R_Evo.tolist(),
+            "coopPayoff_Evo": coopPayoff_Evo.tolist(),
+            "sigCost_Evo": sigCost_Evo.tolist(),
+            "coopCost_Evo": coopCost_Evo.tolist(),
+            "auto_pro_Rate_Evo": auto_pro_Rate_Evo.tolist()}
+    t = time.asctime().replace(":", "-" )
+    with open(f"Wang python\json\\autoreg\\{t} {coop_Cost} {sig_Cost // 10**8} {lam} {mu_Cheats} {auto} {max_G}.json", "w") as f:
+        json.dump(data, f,  ensure_ascii=False, indent=4)
+
+def vary_genotype(lam, Auto):
+    clonal = False
+    # t = time.time_ns()
+    init_SN, sig_Cost, K, mu_Cheats, max_G = 1, 10**9, 50.0, 10.0 ** -4, 5000
+    returnValues = main_QS(init_SN, sig_Cost ,lam , K, mu_Cheats, max_G=max_G, Auto=Auto, clonal=clonal)
+    # fit_Evo, pro_Rate_Evo, sensitivity_Evo, auto_R_Evo, coopPayoff_Evo, sigCost_Evo, coopCost_Evo, auto_pro_Rate_Evo = returnValues
+    # print((time.time_ns()-t)* 10 **-9)
+    # print(f'{sig_Cost // 10 ** 8} done') 
+
+if __name__ == "__main__":
+
+    # joblib.Parallel(n_jobs=6)(joblib.delayed(vary_signal)(sig_Cost * 10**8) for sig_Cost in range(5,105,5))
+    # joblib.Parallel(n_jobs=6)(joblib.delayed(vary_genotype)(np.round(lam, decimals=1), True) for lam in np.arange(0,10,step=.1))
+    # joblib.Parallel(n_jobs=5)(joblib.delayed(vary_genotype)(np.round(lam, decimals=1), False) for lam in np.arange(10,50,step=1))
+    Auto = False
+    clonal = False
+    # # t = time.time_ns()
+    init_SN, sig_Cost, lam, K, max_G = 1, 10**9, 10, 50.0, 5000
+    file = main_QS(init_SN, sig_Cost ,lam , K, max_G=max_G, Auto=Auto, clonal=clonal)
+    # # graphNoThresholds.graph(file)
